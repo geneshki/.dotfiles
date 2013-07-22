62f9f4d5df02f79f39ead5905b0638700cdd0be3
Nikola Geneshki <ngeneshki@gmail.com>
2013-07-22 15:40:34 +0300

    .vimrc and config script and properties are added.

---
+++ .vimrc
@@ -0,0 +1,10 @@
+runtime bundle/vim-pathogen-master/autoload/pathogen.vim
+call pathogen#infect()
+call pathogen#helptags()
+
+
+"execute pathogen#infect()
+syntax on
+filetype plugin indent on
+
+
---
+++ config.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+set -e;
+
+currentDir=$(pwd);
+destination=$( cat config.properties | sed -e '_#.*_ d' -e 's/[ ^I]*$//' -e '/^$/ d' | tail -n +1 |head -n 1);
+cd $destination;
+cat $currentDir/config.properties | sed -e '_#.*_ d' -e 's/[ ^I]*$//' -e '/^$/ d' | tail -n +3 | while read line; do 
+  echo cloning from $line;
+  git clone $line;
+done
+echo Config SUCCESSFUL!
