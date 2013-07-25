1b73c25f135ac11d0a7773820cdfea7639b44b9b
Nikola Geneshki <ngeneshki@gmail.com>
2013-07-25 14:34:22 +0300

    config.sh now checks if dest directory is absolute or relative path. also it exits on error while reading config.properties and does not while cloning plugins. config.properties has new entry showing if the destination path is relative or absolute.

--- config.sh
+++ config.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
-#set -e;
+set -e; #if there are problems with the configuration - exit
 currentDir=$(pwd);
-destination=$( cat config.properties | sed -e '\_#.*_ d' -e 's/[ ^I]*$//' -e '/^$/ d' | tail -n +1 |head -n 1);
+destinationIsAbsolute=$( cat config.properties | set -e '\_#.*_ d' -e 's/[ ^I]*$//' -e '/^$/ d' | tail -n +1 | head -n 1);
+destination=$( cat config.properties | sed -e '\_#.*_ d' -e 's/[ ^I]*$//' -e '/^$/ d' | tail -n +2 |head -n 1);
+if [ "$destinationIsAbsolute" != "true" ]
+then
+ destination="$HOME$destination"
+fi
+echo bundle directory path: $destination;
 cd $destination;
-cat $currentDir/config.properties | sed -e '\_#.*_ d' -e 's/[ ^I]*$//' -e '/^$/ d' | tail -n +2 | while read line; do 
+set +e; # don't exit on error. This way even if plugins are downloaded or there's another error,
+        # the rest of the plugins would be cloned.
+cat $currentDir/config.properties | sed -e '\_#.*_ d' -e 's/[ ^I]*$//' -e '/^$/ d' | tail -n +3 | while read line; do 
   echo cloning from $line;
   git clone $line;
 done
 
 cd ../
-ln -s .dotfiles/.vimrc
+ln -s $currentDir/.vimrc
 #echo Config SUCCESSFUL!
